4fad38291ce9b320248fd8745adc3e0c15fa7567
OP-ACE <pengfei_guo@126.com>
2022-02-26 02:01:55 +0800

    comment out incomplete code

--- CTRP/LimitObjectByCRTP.cpp
+++ CTRP/LimitObjectByCRTP.cpp
@@ -1,28 +1,28 @@
-template <class ToBeLimited, uint32_t maxInstance>
-struct LimitNoOfInstances {
-    static atomic<uint32_t> cnt;
+// template <class ToBeLimited, uint32_t maxInstance>
+// struct LimitNoOfInstances {
+//     static atomic<uint32_t> cnt;
 
-    LimitNoOfInstances() {
-        if (cnt >= maxInstance)
-            throw logic_error{"Too Many Instances"};
-        ++cnt;
-    }
-    ~LimitNoOfInstances() { --cnt; }
-}; // Copy, move & other sanity checks to be complete
+//     LimitNoOfInstances() {
+//         if (cnt >= maxInstance)
+//             throw logic_error{"Too Many Instances"};
+//         ++cnt;
+//     }
+//     ~LimitNoOfInstances() { --cnt; }
+// }; // Copy, move & other sanity checks to be complete
 
-struct One : LimitNoOfInstances<One, 1> {};
-struct Two : LimitNoOfInstances<Two, 2> {};
+// struct One : LimitNoOfInstances<One, 1> {};
+// struct Two : LimitNoOfInstances<Two, 2> {};
 
-template <class T, uint32_t maxNoOfInstace>
-atomic<uint32_t> LimitNoOfInstances<T, maxNoOfInstace>::cnt(0);
+// template <class T, uint32_t maxNoOfInstace>
+// atomic<uint32_t> LimitNoOfInstances<T, maxNoOfInstace>::cnt(0);
 
 
-void use_case() {
-    Two _2_0, _2_1;
+// void use_case() {
+//     Two _2_0, _2_1;
 
-    try {
-        One _1_0, _1_1;
-    } catch (exception &e) {
-        cout << e.what() << endl;
-    }
-}
+//     try {
+//         One _1_0, _1_1;
+//     } catch (exception &e) {
+//         cout << e.what() << endl;
+//     }
+// }
