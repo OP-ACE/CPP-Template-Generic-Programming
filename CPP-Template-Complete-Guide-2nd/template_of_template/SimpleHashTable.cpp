a980c05fb6562601d42beab06b6eec983958021a
OP-ACE <pengfei_guo@126.com>
2022-03-02 12:53:06 +0800

    继续写容器模板

---
+++ CPP-Template-Complete-Guide-2nd/template_of_template/SimpleHashTable.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+template < class Key_Type, class Value_Type >
+class SimpleHashTable
+{   
+    public:
+    void add(Key_Type key, Value_Type val);
+    
+
+    
+}
--- CPP-Template-Complete-Guide-2nd/template_of_template/cpp_template_template.cpp
+++ CPP-Template-Complete-Guide-2nd/template_of_template/cpp_template_template.cpp
@@ -1,81 +1,114 @@
-
-
-template<typename DType>
-class CData
-{
-public:
-    std::vector<DType>    mContent;
-};
-
-
-CData<int> x;
-
-template<typename DType, typename TContainer >
-class CData
-{
-public:
-    TContainer<DType>    mContent;
-};
-
-CData<int,std::vector> x;
-
-template<typename TContainer >
-class CData
-{
-public:
-    TContainer    mContent;
-};
-
-CData< std::vector<int> > x;
-
-
-template<    typename DType,
-            template<typename T> class TContainer >
-class CData
-{
-public:
-    TContainer<DType>    mContent;
-};
-
-template<typename T>
-class CVector : public std::vector<T>
-{};
-
-CData<int, CVector> x;
-
 #include <iostream>
-template<template<typename T> class TFunc>
-void FuncTemp(int iMode)
-{
-    if (iMode == 0 )
-    {
-        TFunc<int>()();
-    }
-    else if ( iMode == 1 )
-    {
-        TFunc<float>()();
-    }
-}
+#include <vector>
 
-template<typename T>
-class CFunc1
+using namespace std;
+
+template <typename T>
+class my_vec_box
 {
 public:
-    void operator()(){
-    }
+    vector<T> content;
 };
 
-template<typename T>
-class CFunc2
+
+
+template <typename ET, typename CT> // ET : element type, CT : container type
+class my_box_wrong
 {
 public:
-    void operator()(){
-    }
+    //CT<ET> content; //! error: 'CT' is not a template
 };
+
+
+
+template<typename CT >
+class my_box_naive
+{
+public:
+    CT    mContent;
+};
+
+
+// 容器类型 和 数据类型 分开指定
+
+
+
+template<typename ET, 
+         template<typename CT> class container >
+class my_box
+{
+public:
+    container<ET>    content;
+  //std::vector<int>
+};
+
+
 
 int main()
 {
-    FuncTemp<CFunc1>(1);
-    FuncTemp<CFunc2>(0);
+    my_vec_box<int> box;
+    vector<int> myvec = box.content;
+    myvec.push_back(1);
+    cout << "-----------" << endl;
+    cout << myvec[0] << endl;
+
+    
+    // my_box_naive< std::vector<int> > vec_int_naivebox;
+
+    cout << "-----------" << endl;
+    my_box<int, std::vector> vec_int_box;
+    // auto vec2 = vec_int_box.content.push_back(1);
+    // cout << vec2[0] << endl;
+
 }
 
+
+// template<    typename DType,
+//             template<typename T> class TContainer >
+// class CData
+// {
+// public:
+//     TContainer<DType>    mContent;
+// };
+
+// template<typename T>
+// class CVector : public std::vector<T>
+// {};
+
+// CData<int, CVector> x;
+
+// #include <iostream>
+// template<template<typename T> class TFunc>
+// void FuncTemp(int iMode)
+// {
+//     if (iMode == 0 )
+//     {
+//         TFunc<int>()();
+//     }
+//     else if ( iMode == 1 )
+//     {
+//         TFunc<float>()();
+//     }
+// }
+
+// template<typename T>
+// class CFunc1
+// {
+// public:
+//     void operator()(){
+//     }
+// };
+
+// template<typename T>
+// class CFunc2
+// {
+// public:
+//     void operator()(){
+//     }
+// };
+
+// int main()
+// {
+//     FuncTemp<CFunc1>(1);
+//     FuncTemp<CFunc2>(0);
+// }
