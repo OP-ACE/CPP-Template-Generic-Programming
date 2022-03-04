20d0a03d9dbd4613a373fb5e8697e487da37c044
OP-ACE <pengfei_guo@126.com>
2022-03-05 00:19:37 +0800

    如何定义和调用: 重载输出运算符

---
+++ CPP-Template-Complete-Guide-2nd/MyUglyCodeKeepsMeMoving/stack_2022_03_04_01_push_pop_top_empty_print.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// 写一个类 叫 Stack<T> 
+// 属性: std::vector<T> v
+// 方法: void  push(T a); 
+//       void  pop(); 
+//       bool  empty();
+//       T     top(); 
+
+
+
+
+template<class T>
+class Stack
+{
+    private:
+    std::vector<T> v;
+
+    public:
+    void  push(T a);
+    void  pop();
+    bool  empty();
+    T     top();
+    void  printOn(std::ostream & os);       //! -------- 001 [错误] 丢了&, 忘记了 传 stream 只能 按引用传
+};
+
+template<class T>
+void Stack<T>::push(T a)
+{                                
+    v.push_back(a);               
+}
+
+template<class T>
+void Stack<T>::pop()
+{                                
+    v.pop_back();
+}
+
+template<class T>
+bool Stack<T>::empty()
+{
+    return v.empty();            
+}
+
+template<class T>
+T Stack<T>::top()
+{                               
+    return v.back();
+}
+
+template<class T>
+void Stack<T>::printOn(std::ostream & os)  //! -------- 002 [重点] 如何重载输出运算符 <<
+{                               
+    for(T const & vi : v)
+    {os << vi << " ";}
+}
+
+int main()
+{
+    //Stack<int> s1{12, 34, 56}; 
+    Stack<int> s1;
+    std::cout << s1.empty() << std::endl;
+    s1.push(12);
+    s1.push(34);
+    s1.push(56);
+    std::cout << s1.top() << std::endl;
+    s1.pop();
+    std::cout << s1.top() << std::endl;
+    std::cout << s1.empty() << std::endl;
+    s1.printOn(std::cout);                 //! --------- 003 [错误] 不知道如何调用重载的输出运算符, 不知该传什么东东给printOn
+}
