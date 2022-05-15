124c797e2b23381a1f5eb32bf2c432cce3df8f9f
OP-ACE <pengfei_guo@126.com>
2022-05-15 12:34:17 +0800

    指向整个数组的指针 和 指向数组首元素的指针 值相同, 但是两个(包含不同内容的)完全不同的指针

--- CPP-Template-Complete-Guide-2nd/chap08 元编程/指向二维数组的指针.cpp
+++ CPP-Template-Complete-Guide-2nd/chap08 元编程/指向二维数组的指针.cpp
@@ -3,26 +3,24 @@
 
 using namespace std;
 
-int a[3][4] = {{0, 1,  2,  3},
-               {4, 5,  6,  7},
-               {8, 9, 10, 11}};
+int a[2][3] = {{0, 1,  2},   // p     指向第 1行 矢量(子矩阵)
+               {3, 4,  5} }; // p + 1 指向第 2行 矢量(子矩阵)
 
 int main(){
     
-    int (*p)[4] = a;
+    int (*p)[3] = a;
 
-    cout << " p       =  " <<  p           << endl;
-    cout << " &p      =  " <<  &p          << endl;
-    // p = 1;
-    p++;
+    cout << "\n---- p points to a[0] (a[0][0])\n";
+    cout << " p         =  " <<  p           << endl;
+    cout << " a         =  " <<  a           << endl;
+    cout << " &a[0]     =  " <<  &a[0]       << endl;
+    cout << " &a[0][0]  =  " <<  &a[0][0]    << endl;
 
-    cout << " a       =  " <<  a           << endl;
-    cout << " a[0]    =  " <<  a[0]        << endl;
-    cout << " *(a[0]) =  " <<  *(a[0])     << endl;
-  
-    cout << " a[1]    =  " <<  a[1]        << endl;
-    cout << " (p+1)   =  " <<  *(p + 1)    << endl;
-    cout << " (a+1)   =  " <<  *(a + 1)    << endl;
+    cout << "\n---- p+1 points to a[1] (a[1][0])\n";
+    cout << " **(p + 1)         =  " <<  **(p + 1)          << endl;
+    cout << " a + 1         =  " <<  a + 1         << endl;
+    cout << " &a[0]     =  " <<  &a[0]       << endl;
+    cout << " &a[0][0]  =  " <<  &a[0][0]    << endl;
 
 
 
