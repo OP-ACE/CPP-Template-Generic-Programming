e4e915fa4fc06f23b19d0e0731aea62cd892adf7
OP-ACE <pengfei_guo@126.com>
2022-06-09 17:58:50 +0800

    polymorph-learncpp.com

--- CPP-Template-Complete-Guide-2nd/chap18-模板的多态性/dyna_poly.cpp
+++ CPP-Template-Complete-Guide-2nd/chap18-模板的多态性/dyna_poly.cpp
@@ -1,82 +1,47 @@
 #include <iostream>
-#include <vector>
-#include <cstddef>  // size_t
-#include <cstdlib>
-
-class Coord {
-  private:
-    int x, y;
-  public:
-    Coord (int i1, int i2) : x(i1), y(i2) {
+    using namespace std;
+ 
+    class Base
+    {
+       public:
+           virtual void Print() = 0;
+           virtual ~Base(){}
+    };
+ 
+    class child_1 : public Base
+    {
+       public:
+        void Print()
+        {
+            cout << "child_1 Print function" << endl;
+        }
+        ~child_1()
+        {
+            cout << "child_1 destructor function" << endl;
+        }
+    };
+ 
+    class child_2: public Base
+    {
+        public:
+            void Print()
+            {
+                cout << "child_2 Print function" << endl;
+            }
+            ~child_2()
+            {
+                cout << "child_2 destructor function" << endl;
+            }
+    };
+ 
+    int main()
+    {
+        Base *p = new child_1; //父类指针指向子类对象
+        p->Print();
+        delete p;  //记住释放,否则内存泄露
+        p = new child_2;
+        p->Print();
+        delete p;
+        p = NULL;
+        return 0;
     }
-    friend Coord operator- (Coord const& c1, Coord const& c2) {
-        return Coord(c1.x-c2.x, c1.y-c2.y);
-    }
-    Coord abs() {
-        return Coord(std::abs(x),std::abs(y));
-    }
-};
-
-// common abstract base class GeoObj for geometric objects
-class GeoObj {
- public:
-  // draw geometric object:
-  virtual void draw() const = 0;
-  // return center of gravity of geometric object:
-  virtual Coord center_of_gravity() const = 0;
-  //...
-  virtual ~GeoObj() = default;
-};
-
-// concrete geometric object class Circle
-// - derived from GeoObj
-class Circle : public GeoObj {
- public:
-  virtual void draw() const override;
-  virtual Coord center_of_gravity() const override;
-  //...
-};
-
-// concrete geometric object class Line
-// - derived from GeoObj
-class Line : public GeoObj {
- public:
-  virtual void draw() const override;
-  virtual Coord center_of_gravity() const override;
-  //...
-};
-//...
-
-// draw any GeoObj
-void myDraw(GeoObj const& obj) {
-  obj.draw();  // call draw() according to type of object
-}
-
-// compute distance of center of gravity between two GeoObjs
-Coord distance(GeoObj const& x1, GeoObj const& x2) {
-  Coord c = x1.center_of_gravity() - x2.center_of_gravity();
-  return c.abs();  // return coordinates as absolute values
-}
-
-// draw heterogeneous collection of GeoObjs
-void drawElems(std::vector<GeoObj*> const& elems) {
-  for (std::size_type i = 0; i < elems.size(); ++i) {
-    elems[i]->draw();  // call draw() according to type of element
-  }
-}
-
-int main() {
-  Line l;
-  Circle c, c1, c2;
-
-  myDraw(l);  // myDraw(GeoObj\&) => Line::draw()
-  myDraw(c);  // myDraw(GeoObj\&) => Circle::draw()
-
-  distance(c1, c2);  // distance(GeoObj\&,GeoObj\&)
-  distance(l, c);    // distance(GeoObj\&,GeoObj\&)
-
-  std::vector<GeoObj*> coll;  // heterogeneous collection
-  coll.push_back(&l);         // insert line
-  coll.push_back(&c);         // insert circle
-  drawElems(coll);            // draw different kinds of GeoObjs
-}
